844382c77833776e454350b0bf10e7b65f73f8cd
«lepazh» <lepazhpol@gmail.com>
2022-07-14 20:17:44 +0300

    added solution 1

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,42 @@
+﻿// На вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+//Вариант 1
+
+//string? imputLkneOne = Console.ReadLine();
+//string? imputLkneTwo = Console.ReadLine();
+//
+//if(imputLkneOne != null && imputLkneTwo != null)
+//{
+//   int imputNumberOne = int.Parse(imputLkneOne);
+//    int imputNumberTwo = int.Parse(imputLkneTwo);
+//
+//    if(imputNumberOne == imputNumberTwo * imputNumberTwo)
+//   {
+//        Console.WriteLine("yes");
+//    }
+//    else
+//    {
+//        Console.WriteLine("no");
+//    }
+//}
+
+//Вариант 2
+
+string? imputLkneOne = Console.ReadLine();
+string? imputLkneTwo = Console.ReadLine();
+
+if(imputLkneOne != null && imputLkneTwo != null)
+{
+   int imputNumberOne = int.Parse(imputLkneOne);
+   int imputNumberTwo = int.Parse(imputLkneTwo);
+
+    if(imputNumberTwo == Math.Sqrt(imputNumberOne))
+   {
+        Console.WriteLine("yes");
+   }
+   else
+   {
+       Console.WriteLine("no");
+   }
+
+}
