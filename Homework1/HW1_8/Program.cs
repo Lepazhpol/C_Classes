bbd6e66280693b0888ebe038fad53bb1703afc53
«lepazh» <lepazhpol@gmail.com>
2022-07-14 23:04:46 +0300

    added HW1_8

---
+++ Homework1/HW1_8/Program.cs
@@ -0,0 +1,21 @@
+﻿// На вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+string? imputLine = Console.ReadLine();
+
+if(imputLine != null)
+{
+    int imputNumber = int.Parse(imputLine);
+    int startNumber = 1;
+    string lineOutput = "";
+    
+    while (startNumber < imputNumber)
+    {
+        lineOutput = lineOutput+startNumber+", ";
+        startNumber++;
+    }
+
+    lineOutput = lineOutput+imputNumber;
+
+    Console.WriteLine(lineOutput);
+
+}
