461a6bcad88e8b1c62d137b242d7bddd149fa6b5
«lepazh» <lepazhpol@gmail.com>
2022-07-14 21:25:54 +0300

    added solution 3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Введите число");
+string? imputLkneOne = Console.ReadLine();
+Console.WriteLine("Спасибо");
+
+if(imputLkneOne != null)
+{
+    int imputNumber = int.Parse(imputLkneOne);
+    // int startNumber = (imputNumber * -1);
+    // string lineOutput = "";
+    
+    // while (startNumber < imputNumber);
+    // {
+    //     lineOutput = lineOutput+startNumber+", ";
+    //     startNumber++;
+    // }
+
+    // lineOutput = lineOutput+imputNumber;
+
+    // Console.WriteLine(lineOutput);
+
+    string lineOutput = "0";
+    
+    while (startNumber <= imputNumber);
+    {
+        lineOutput = startNumber*(-1)+", "+lineOutput+", "+startNumber;
+        startNumber++;
+    }
+
+
+    Console.WriteLine(lineOutput);
+
+}
+Console.WriteLine("Конец программы");
