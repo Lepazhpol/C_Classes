65f58b041c6e75853c55c49f20780fdf7fa1b200
«lepazh» <lepazhpol@gmail.com>
2022-07-14 19:44:23 +0300

    add first solution

---
+++ SolutionTask0/Program.cs
@@ -0,0 +1,13 @@
+﻿//Квадрат вводимого числа
+ 
+ 
+string? imputLine = Console.ReadLine();
+if(imputLine != null)
+{
+   int inputNumber = int.Parse(imputLine);
+ 
+  // int outNumber = inputNumber*inputNumber;
+   int outNumber = (int)Math.Pow(inputNumber,2);
+ 
+   Console.WriteLine(outNumber);
+}
