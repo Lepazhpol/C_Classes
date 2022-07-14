3f924865f5d9c9167cc171be4e4813473a61b275
«lepazh» <lepazhpol@gmail.com>
2022-07-14 22:55:05 +0300

    added HW1_6

---
+++ Homework1/HW1_6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Принимает число и опеределяет является ли оно четным или нет
+
+string? imputLineA = Console.ReadLine();
+
+if((imputLineA) != null)
+{
+
+   int inputNumberA = int.Parse(imputLineA);
+   
+   if((inputNumberA%2)==0)
+   {
+    Console.WriteLine("yes");
+   }
+   else
+   {
+    Console.WriteLine("no");
+   }
+
+
+}
