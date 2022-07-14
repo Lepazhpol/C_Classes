7369fe4f8431f23d2f25ba3a05a5c5f64b64810b
«lepazh» <lepazhpol@gmail.com>
2022-07-14 22:29:53 +0300

    added HW1_2

---
+++ Homework1/HW1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимает 2 числа и выдает какое число больше, а какое меньше
+
+string? imputLineA = Console.ReadLine();
+string? imputLineB = Console.ReadLine();
+
+if((imputLineA+imputLineB) != null)
+{
+
+   int inputNumberA = int.Parse(imputLineA);
+   int inputNumberB = int.Parse(imputLineB);
+
+   if(inputNumberA>inputNumberB)
+   {
+    Console.Write("max = ");
+    Console.WriteLine(inputNumberA);
+   }
+   else
+   {
+    Console.Write("max = ");
+    Console.WriteLine(inputNumberB);
+   }
+
+}
