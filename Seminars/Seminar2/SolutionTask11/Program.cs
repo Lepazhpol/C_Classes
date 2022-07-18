b938fd5911cee6f56722f154a12e5ce4314c5318
«lepazh» <lepazhpol@gmail.com>
2022-07-18 18:36:34 +0300

    rename folders and added Task 14

--- Seminars/Seminar2/SolutionTask11/Program.cs
+++ Seminars/Seminar2/SolutionTask11/Program.cs
@@ -1,22 +1,26 @@
-﻿// принмает два числа, выводит, является ли второе число кратным первому. Если второе число некратно первому, то выводит отстаток от деления
-Console.WriteLine("Введите первое число: ");
-string? num1 = Console.ReadLine();
-Console.WriteLine("Введите второе число: ");
-string? num2 = Console.ReadLine();
+﻿// выводит случайное трехзначное число и удаляет вторую цифру этого числа
 
-if (num1 != null && num2 != null)
+// Вариант 1
+
+void solutionVithDigits()
 {
-    int numInt1 = int.Parse(num1);
-    int numInt2 = int.Parse(num2);
-
-    int div = numInt2 % numInt1;
-
-    if (div == 0)
-    {
-        Console.WriteLine("Да");
-    }
-    else
-    {
-        Console.WriteLine("Нет" + "Остаток: " + div);
-    }
+    Console.WriteLine("Вариант 1");
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine("Число: " + number);
+    int numberSecondDigit = number / 100;
+    int numberThirdDigit = number % 10;
+    Console.WriteLine("Число после удаления второй цифры: " + numberSecondDigit + numberThirdDigit);
 }
+
+void solutionVithStrings()
+{
+    int numberS = new Random().Next(100, 1000);
+    Console.WriteLine("Случайное число: " + numberS);
+    string stringNum = numberS.ToString();
+    Console.WriteLine(stringNum);
+    Console.Write(stringNum[0]);
+    Console.Write(stringNum[2]);
+}
+
+solutionVithDigits();
+solutionVithStrings();
