9bb66c6dd654c77b63ccd68ef735850a5394fd8a
«lepazh» <lepazhpol@gmail.com>
2022-07-18 18:58:35 +0300

    added solution Task 14

--- Seminars/Seminar2/SolutionTask14/Program.cs
+++ Seminars/Seminar2/SolutionTask14/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает на вход число и проверяет кратно ли оно одновременно и 7 и 23
+
+string reminderSearcher(string? str)
+{
+    if (str != null)
+    {
+        try                     
+        {
+            string answer;
+            int num = int.Parse(str);
+            if (((num % 7) == 0) && ((num % 23) == 0))
+            {
+                answer = "кратно";
+            }
+            else
+            {
+                answer = "Не кратно";
+            }
+            return answer;
+        }
+        catch (Exception e)                 
+        {
+            return "Возникло исключение: " + e.Message;             
+        }
+
+    }
+    return "";
+}
+
+
+Console.WriteLine("Введите число: ");
+
+string? imputOne = Console.ReadLine();
+
+Console.WriteLine(reminderSearcher(imputOne));
