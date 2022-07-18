f83dc3e655b1841d5f49bd765ca970bf3a2a5608
«lepazh» <lepazhpol@gmail.com>
2022-07-18 18:11:44 +0300

    added Task10

---
+++ Seminars/Seminar2/SolutionTask10/Program.cs
@@ -0,0 +1,26 @@
+﻿// выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+// Вариант 1
+
+void solutionVithDigits()
+{
+    Console.WriteLine("Вариант 1");
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine("Число: " + number);
+    int numberSecondDigit = number / 100;
+    int numberThirdDigit = number % 10;
+    Console.WriteLine("Число после удаления второй цифры: " + numberSecondDigit + numberThirdDigit);
+}
+
+void solutionVithStrings()
+{
+    int numberS = new Random().Next(100, 1000);
+    Console.WriteLine("Случайное число: " + numberS);
+    string stringNum = numberS.ToString();
+    Console.WriteLine(stringNum);
+    Console.Write(stringNum[0]);
+    Console.Write(stringNum[2]);
+}
+
+solutionVithDigits();
+solutionVithStrings();
