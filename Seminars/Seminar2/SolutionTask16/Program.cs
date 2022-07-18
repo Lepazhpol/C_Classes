d0bdd308ba443465d236c3425c9525c9d4b6af44
«lepazh» <lepazhpol@gmail.com>
2022-07-18 19:16:12 +0300

    added solution Task 16

---
+++ Seminars/Seminar2/SolutionTask16/Program.cs
@@ -0,0 +1,24 @@
+﻿// принимает на вход два числа и проверяет, является ли одно число квадратом другого
+Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+string? imputFirst = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string? imputSecond = Console.ReadLine();
+
+if((imputFirst != null) && (imputSecond != null))
+{
+    int imputNumberFirst = int.Parse(imputFirst);
+    int imputNumberSecond = int.Parse(imputSecond);
+    int a = imputNumberFirst * imputNumberFirst;
+    int b = imputNumberSecond * imputNumberSecond;
+    if ((a == imputNumberSecond) || (b == imputNumberSecond))
+    {
+        Console.WriteLine("Yes!");
+    }
+    else
+    {
+        Console.WriteLine("No!");
+
+    }
+}
