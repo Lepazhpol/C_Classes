d94ba5bd676f1cd589822c95ce77eee610cbc690
«lepazh» <lepazhpol@gmail.com>
2022-07-18 22:36:35 +0300

    do beautiful and handing over homework 2

--- Homeworks/Homework2/HW2_13/Program.cs
+++ Homeworks/Homework2/HW2_13/Program.cs
@@ -4,10 +4,10 @@ Console.Clear();
 
 Console.WriteLine("Введите число для поиска в нем третьей цифры: ");
 string numStr = Console.ReadLine();
-if (numStr != null) //проверяем на пустоту
+if (numStr != null)                                                     //проверяем на пустоту
 {
-    char[] numArray = numStr.ToCharArray(); //создаем массив
-    if (numArray.Length >= 3)   //проверяем, чтобы цифр в числе было не меньше трех
+    char[] numArray = numStr.ToCharArray();                             //создаем массив
+    if (numArray.Length >= 3)                                           //проверяем, чтобы цифр в числе было не меньше трех
     {
         Console.WriteLine("Третья цифра этого числа: " + numArray[2]);  //выводим третий элемент массива
     }
