4918bf92f687c937b863f4fab558f1e4e04ef3c1
«lepazh» <lepazhpol@gmail.com>
2022-07-18 22:31:20 +0300

    added HW 2_15 and handing over homework 2

--- Homeworks/Homework2/HW2_10/Program.cs
+++ Homeworks/Homework2/HW2_10/Program.cs
@@ -2,24 +2,31 @@
 
 Console.Clear();
 
-Console.WriteLine("Вы хотите самостоятельно ввести число? y/n: ");
+Console.WriteLine("Вы хотите самостоятельно ввести число? y/n: ");      //заморачиваемся и даем выбор, хотя и не просили
 string answer = Console.ReadLine();
-if (answer != null)
+if (answer != null)                                                     //проверяем на пустоту
 {
-    if (answer.ToLower() == "y")
+    if (answer.ToLower() == "y")                                        //проверяем ответ пользователя
     {
         Console.WriteLine("Введите трехзначное число: ");
         string stringNum = Console.ReadLine();
         if (stringNum != null)
         {
-        Console.WriteLine(stringNum[1]);
+            if (stringNum.Length == 3)                                  //проверяем, чтоб число было трехзначным
+            {
+                Console.WriteLine(stringNum[1]);                        //выводим второй элемент строки
+            }
+            else
+            {
+                Console.WriteLine("Вот и давай после этого свободу людям... Просили же трехзначное. Когда машины восстанут, Вы будете первым...");
+            }                                
         }
     }
     else
     {
-        int number = new Random().Next(100, 1000);
+        int number = new Random().Next(100, 1000);                      //генерируем случайное трехзначное число
         Console.WriteLine("Случайное трехзначное число: " + number);
-        string stringNum = number.ToString();
+        string stringNum = number.ToString();                           //преобразуем в строку, потому что решаем через строки
         Console.WriteLine(stringNum[1]);
     }
 }
--- Homeworks/Homework2/HW2_13/Program.cs
+++ Homeworks/Homework2/HW2_13/Program.cs
@@ -4,12 +4,12 @@ Console.Clear();
 
 Console.WriteLine("Введите число для поиска в нем третьей цифры: ");
 string numStr = Console.ReadLine();
-if (numStr != null)
+if (numStr != null) //проверяем на пустоту
 {
-    char[] numArray = numStr.ToCharArray();
-    if (numArray.Length >= 3)
+    char[] numArray = numStr.ToCharArray(); //создаем массив
+    if (numArray.Length >= 3)   //проверяем, чтобы цифр в числе было не меньше трех
     {
-        Console.WriteLine("Третья цифра этого числа: " + numArray[2]);
+        Console.WriteLine("Третья цифра этого числа: " + numArray[2]);  //выводим третий элемент массива
     }
     else
     {
