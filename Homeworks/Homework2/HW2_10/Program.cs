8974adf63c039d6e5b0143001f89af24273b0399
«lepazh» <lepazhpol@gmail.com>
2022-07-18 21:32:10 +0300

    added HW 2_10

---
+++ Homeworks/Homework2/HW2_10/Program.cs
@@ -0,0 +1,20 @@
+﻿// принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Вы хотите самостоятельно ввести число? y/n: ");
+string answer = Console.ReadLine();
+if (answer != null)
+{
+    if (answer.ToLower() == "y")
+    {
+        Console.WriteLine("Введите трехзначное число: ");
+        string stringNum = Console.ReadLine();
+        Console.WriteLine(stringNum[1]);
+    }
+    else
+    {
+        int number = new Random().Next(100, 1000);
+        Console.WriteLine("Случайное трехзначное число: " + number);
+        string stringNum = number.ToString();
+        Console.WriteLine(stringNum[1]);
+    }
+}
