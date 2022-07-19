be9aec5400b079b7fe7de8aa4e0b269ffdd41c71
«lepazh» <lepazhpol@gmail.com>
2022-07-19 15:35:15 +0300

    added Task18

---
+++ Seminars/Seminar3/SolutionTask18/Program.cs
@@ -0,0 +1,19 @@
+﻿// по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (х и у)
+
+void printAnswer(int number)
+{
+    if(number == 1) Console.WriteLine("Допустимо: x>0, y>0");
+    if(number == 2) Console.WriteLine("Допустимо: x<0, y>0");
+    if(number == 3) Console.WriteLine("Допустимо: x<0, y<0");
+    if(number == 4) Console.WriteLine("Допустимо: x>0, y,0");
+}
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int imputNumber = int.Parse(inputLine);
+
+    printAnswer(imputNumber);
+
+}
