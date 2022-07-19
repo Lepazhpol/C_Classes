8fc899f05590dfe3ea09a3835fb8b112904435e9
«lepazh» <lepazhpol@gmail.com>
2022-07-19 16:22:43 +0300

    added Task22

---
+++ Seminars/Seminar3/SolutionTask22/Program.cs
@@ -0,0 +1,20 @@
+﻿// принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N
+
+string? imputLineN = Console.ReadLine();
+
+if (imputLineN != null)
+{
+    int numberN = int.Parse(imputLineN);
+    string lineN = "";
+    string lineNN = string.Empty;
+    int s = 1;
+
+    while (s < numberN)
+    {
+        lineN = lineN + s + " ";
+        lineNN = lineNN + (s * s).ToString() + " ";
+        s++;
+    }
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNN);
+}
