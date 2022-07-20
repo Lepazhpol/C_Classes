fc2df24d5c77000ce735c5d0ae7c3ffae4b1cb4a
«lepazh» <lepazhpol@gmail.com>
2022-07-20 18:03:59 +0300

    do nice and send homework 3

--- Homeworks/Homework3/HW3_23/Program.cs
+++ Homeworks/Homework3/HW3_23/Program.cs
@@ -5,17 +5,19 @@ string? imputLineN = Console.ReadLine();
 
 if (imputLineN != null)
 {
-    int numberN = int.Parse(imputLineN);
-    string lineN = "";
-    string lineNNN = string.Empty;
+    int numberN = int.Parse(imputLineN);                        //Парсим
+    string lineN = "";                                          //пустоту можно и так
+    string lineNNN = string.Empty;                              //и так
     int s = 1;
 
     while (s <= numberN)
     {
         lineN = lineN + s + " ";
-        lineNNN = lineNNN + (s * s * s).ToString() + " ";
+        lineNNN = lineNNN + (s * s * s).ToString() + " ";       //пока у нас не наше число накапливаем и состав числа и кубы
         s++;
     }
     Console.WriteLine(lineN);
     Console.WriteLine(lineNNN);
 }
+
+// таблицу очень долго пытался, но, похоже чего-то мне не хватает(((
