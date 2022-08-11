df619013b4996d01343b3add5f42a1da466c0f81
«lepazh» <lepazhpol@gmail.com>
2022-08-11 22:01:00 +0300

    added lection 4

---
+++ Lections/Lection4/Example15/Program.cs
@@ -0,0 +1,14 @@
+﻿//считает факториал. рекурсия, переполнение
+
+double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i = 1; i < 1000; i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}");
+}
