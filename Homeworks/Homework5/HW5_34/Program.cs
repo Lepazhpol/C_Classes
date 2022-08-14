4255ce58f18efc1fc03a8215dbf893115b406d00
«lepazh» <lepazhpol@gmail.com>
2022-08-14 22:58:22 +0300

    added HW5 solutions 34 and 36

---
+++ Homeworks/Homework5/HW5_34/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Clear();
+int[] bufferArray = FillingArray();
+PrintArray(bufferArray);
+Console.WriteLine("Количество четных элементов в массиве: " + CalculateTask(bufferArray));
+
+
+
+//Метод возвращает массив заполненный случайными числами
+int[] FillingArray()
+{
+    Console.WriteLine("Укажите размер массива: ");
+    string imputLengthLine = Console.ReadLine() ?? "";
+    int imputLength = int.Parse(imputLengthLine);
+    int[] outArray = new int[imputLength];
+    int i = 0;
+    System.Random umberSintezator = new System.Random();
+    while (i < imputLength)
+    {
+        outArray[i] = umberSintezator.Next(100, 1000);
+        i++;
+    }
+    return outArray;
+}
+
+//Метод печатающий масиив
+void PrintArray(int[] inputArray)
+{
+    int i = 0;
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write(inputArray[i] + ", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+//Метод производящий решение
+int CalculateTask(int[] inputArray)
+{
+    int amount = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if ((inputArray[i] % 2) == 0) amount++;
+    }
+    return amount;
+}
---
+++ Homeworks/Homework5/HW5_36/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях
+
+Console.Clear();
+int[] bufferArray = FillingArray();
+PrintArray(bufferArray);
+Console.WriteLine("Сцмма эелементов стоящих на четной позиции: " + CalculateTask(bufferArray));
+
+
+
+
+//Метод возвращает массив заполненный случайными числами
+int[] FillingArray()
+{
+    Console.WriteLine("Укажите размер массива: ");
+    string imputLengthLine = Console.ReadLine() ?? "";
+    int imputLength = int.Parse(imputLengthLine);
+    int[] outArray = new int[imputLength];
+    int i = 0;
+    System.Random umberSintezator = new System.Random();
+    while (i < imputLength)
+    {
+        outArray[i] = umberSintezator.Next(100, 1000);
+        i++;
+    }
+    return outArray;
+}
+
+//Метод печатающий масиив
+void PrintArray(int[] inputArray)
+{
+    int i = 0;
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write(inputArray[i] + ", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+//Метод производящий решение
+int CalculateTask(int[] inputArray)
+{
+    int sum = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if ((i % 2) == 0)
+        {
+            sum = sum + inputArray[i];
+        }
+    }
+    return sum;
+}
