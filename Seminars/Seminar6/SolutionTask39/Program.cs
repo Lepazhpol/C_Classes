c7eb9e3df39e5342a7113109558b823474cf488a
«lepazh» <lepazhpol@gmail.com>
2022-08-18 22:08:18 +0300

    add seminar5, ST 39, 40

---
+++ Seminars/Seminar6/SolutionTask39/Program.cs
@@ -0,0 +1,64 @@
+﻿// Переворачивает одномерный массив (последний элемент будет на первом месте, а первый - на последнем)
+
+int[] testArray = FillingArray(13);
+PrintArray(testArray);
+int[] reversedArray = ReverseNewArray(testArray);
+PrintArray(reversedArray);
+PrintArray(ReverseSwapArray(testArray));
+
+
+
+
+
+
+//Метод возвращает массив заполненный случайными числами
+int[] FillingArray(int arrayLength)
+{
+    int[] outArray = new int[arrayLength];
+    int i = 0;
+    System.Random umberSintezator = new System.Random();
+    while (i < 13)
+    {
+        outArray[i] = umberSintezator.Next(-1000, 1000);
+        i++;
+    }
+    return outArray;
+}
+
+//Метод печатающий масиив
+void PrintArray(int[] inputArray)
+{
+    int i = 0;
+    while (i < inputArray.Length - 1)
+    {
+        Console.Write(inputArray[i] + ", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+//Метод переворачивающий массив созданием нового массива
+int[] ReverseNewArray(int[] inputArray)
+{
+    int length = inputArray.Length;
+    int[] newArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        newArray[length - 1 - i] = inputArray[i];
+    }
+    return newArray;
+}
+
+//Метод переворачивающий массив без создания новго массива
+int[] ReverseSwapArray(int[] inputArray)
+{
+    int length = inputArray.Length;
+    int buf;
+    for (int i = 0; i < length / 2; i++)
+    {
+        buf = inputArray[i];
+        inputArray[i] = inputArray[length - 1 - i];
+        inputArray[length - 1 - i] = buf;
+    }
+    return inputArray;
+}
---
+++ Seminars/Seminar6/SolutionTask40/Program.cs
@@ -0,0 +1,32 @@
+﻿//Принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины
+
+int sideA = 0;
+int sideB = 0;
+int sideC = 0;
+
+ReadSides();
+bool res = TestTriangle(sideA, sideB, sideC);
+PrintAnswer(res);
+
+void ReadSides()
+{
+    Console.WriteLine("Введите длину первого отрезка: ");
+    sideA = int.Parse(Console.ReadLine() ?? "");
+    Console.WriteLine("Введите длину второго отрезка: ");
+    sideB = int.Parse(Console.ReadLine() ?? "");
+    Console.WriteLine("Введите длину третьего отрезка: ");
+    sideC = int.Parse(Console.ReadLine() ?? "");
+}
+
+bool TestTriangle(int sideA, int sideB, int sideC)
+{
+    bool answer = ((sideA + sideB > sideC) && (sideB + sideC > sideB) && (sideA + sideC > sideB)) ? true : false;
+    // переменная = (условие) ? значение 1 : значение 2;
+    return answer;
+}
+
+void PrintAnswer(bool answer)
+{
+    if (answer) Console.WriteLine("Из эти отрезков можно составить треугольник");
+    else Console.WriteLine("Из эти отрезков нельзя составить треугольник");
+}
