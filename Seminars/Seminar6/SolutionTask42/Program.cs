1c65604892d8a965066866a21ff9dea444a38933
«lepazh» <lepazhpol@gmail.com>
2022-08-19 22:11:47 +0300

    add 42 44

---
+++ Seminars/Seminar6/SolutionTask42/Program.cs
@@ -0,0 +1,9 @@
+﻿// преобразует десятичное число в двоичное
+
+Console.WriteLine(Convert.ToString(int.Parse(Console.ReadLine()),2));
+
+//int.Parse(Console.ReadLine()) - считываем введенное число, парсим в интеджер
+
+//Convert.ToString(int число, 2) - где 2 основание, разрядность системы в которую переводится число. основание только 2, 8, 16
+
+//
---
+++ Seminars/Seminar6/SolutionTask44/Program.cs
@@ -0,0 +1,32 @@
+﻿//не используя рекурсию выведете первые N чисел Фибоначчи. Первые два числа 0 и 1
+
+Console.WriteLine("Введите количество чисел в ряде Фибоначчи: ");
+int decemalNumber = ReadDecemalNumber();
+PrintAnswer(Fibonacci(decemalNumber));
+
+int ReadDecemalNumber()
+{
+    return int.Parse(Console.ReadLine() ?? "");
+}
+
+string Fibonacci(int n)
+{
+    string answer = "";
+    if (n == 2) answer = "0 1";
+    else if (n == 1) answer = 0;
+    else
+    {
+        answer = "0 1";
+        int fn_1 = 1;
+        int fn_2 = 0;
+        int fn;
+        for (int i=0; i < n - 2; i++)
+        {
+            fn = fn_1 + fn_2;
+            answer += " " + fn;
+            fn_2 = fn_1;
+            fn_1 = current;
+        }
+    }
+
+}
