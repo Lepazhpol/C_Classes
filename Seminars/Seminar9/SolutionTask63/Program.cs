3f01e88acc0da3b571661bdfa96f1303762d4b07
«lepazh» <lepazhpol@gmail.com>
2022-08-28 13:56:54 +0300

    added seminar 9

---
+++ Seminars/Seminar9/SolutionTask63/Program.cs
@@ -0,0 +1,35 @@
+﻿
+ /* =================================================================
+ Задайте значение N. 
+ Напишите программу, которая выведет все натуральные числа от 1 до N
+ ==================================================================*/
+
+Console.Clear();
+
+ int ReadData()
+ {
+     Console.Write("Задайте число: ");
+     return int.Parse(Console.ReadLine());
+ }
+
+
+ int NuturalNumberPrinter(int number)
+ {    
+     if (number == 2) return 1; // остановка метода. крайний случай в рекурсии. условие остановки рекурсии 
+     else
+     {
+         // уменьшаем число на 1
+         number--;
+         // печатает ход рекурсии
+         Console.WriteLine(number);
+         // вызываем метод внутри метода (метод вызывает сам себя), но с уменьшенным на 1 числом
+         Console.Write(NuturalNumberPrinter(number) + " ");
+     }
+     return number;
+ }
+
+ int inputNumber = ReadData();
+ // +2 чтобы включить предпоследнее и последнее числа в ряду 
+ NuturalNumberPrinter(inputNumber+2);
+
+
---
+++ Seminars/Seminar9/SolutionTask67/Program.cs
@@ -0,0 +1,26 @@
+﻿/* ======================================================
+ Напишите программу, которая 
+ будет принимать на вход число и возвращать сумму его цифр.
+ =========================================================*/
+
+ // метод считывания данных
+ int ReadData(string line)
+ {
+     Console.Write(line);
+     return Convert.ToInt32(Console.ReadLine());
+ }
+
+
+ // метод вычисления суммы цмфр в числе (рекурсия)
+ int SumDigit(int num)
+ {
+     if (num > 0)
+         return num % 10 + SumDigit(num / 10);
+
+     else
+         return 0;
+ }
+
+
+ int inputNumber = ReadData("Задайте число: ");
+ Console.WriteLine(SumDigit(inputNumber));
